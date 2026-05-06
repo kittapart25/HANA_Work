2c03303894679d435db27cf9e225ccee0a722781
kittapart25 <kittapart@live.rmutl.ac.th>
2026-05-06 20:52:38 +0700

    update ramrate

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function calculate() {
       const coldTempNum = parseFloat(document.getElementById('coldTemp').value) || 0;
       const hotTempNum = parseFloat(document.getElementById('hotTemp').value) || 0;
       const deltaTemp = Math.abs(hotTempNum - coldTempNum);
-      const minTransferTime = deltaTemp / 25;
+      const minTransferTime = deltaTemp / 23;
       
       if (transfer < minTransferTime) {
         alert(`แจ้งเตือน: กรุณาแก้ไข Condition\n\nTransfer time ที่คุณกำหนด (${transfer} นาที) ต่ำเกินไป\nอิงตาม Ramp rate ของเครื่อง TMCL (25 องศา/นาที) คุณต้องใช้เวลา Transfer ขั้นต่ำ ${minTransferTime.toFixed(1)} นาที`);
